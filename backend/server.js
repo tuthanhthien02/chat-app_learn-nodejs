8f83973931e0d0cce20d4d887420caf193b61366
Thanh Thien <tuthanhthien02@gmail.com>
2024-03-24 18:55:42 +0700

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -8,9 +9,11 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -19,9 +22,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//     res.send("hello world");
-// });
+// Serve static assets if in production
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// Serve the index.html file if in production
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}/ `);
